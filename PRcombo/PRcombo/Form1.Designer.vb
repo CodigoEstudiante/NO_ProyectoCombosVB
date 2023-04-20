c9c53e5ae5baa7281e5adc5a65bd94adb87e1fc4
Codigo Estudiante <jchambi97@hotmail.com>
2023-04-19 23:23:54 -0500

    first commit

---
+++ PRcombo/PRcombo/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(34, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(172, 28)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(221, 12)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(176, 28)
+        Me.ComboBox2.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(37, 62)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(168, 160)
+        Me.ListBox1.TabIndex = 2
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(229, 62)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(168, 160)
+        Me.ListBox2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(456, 261)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+
+End Class
---
+++ PRcombo/PRcombo/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Dim con As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\casa\Desktop\Base de datos\sistema.mdb")
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            con.Open()
+            MessageBox.Show("Exito al conectar")
+        Catch ex As Exception
+            MessageBox.Show("error al iniciar", "Mensaje del sistema", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+        loadingCombo()
+        loadinglistbox()
+    End Sub
+
+    Public Sub loadingCombo()
+        Dim table As New DataTable
+        Dim sql As String = "SELECT IdDepartamento,nombre FROM departamentos"
+        Dim adp As New OleDbDataAdapter(sql, con)
+
+        adp.Fill(table)
+
+        ComboBox1.DataSource = table
+        ComboBox1.DisplayMember = "nombre"
+        ComboBox1.ValueMember = "IdDepartamento"
+
+
+    End Sub
+    Public Sub loadinglistbox()
+        Dim table As New DataTable
+        Dim sql As String = "SELECT IdDepartamento, nombre FROM departamentos"
+        Dim adp As New OleDbDataAdapter(sql, con)
+        adp.Fill(table)
+
+        ListBox1.DataSource = table
+        ListBox1.DisplayMember = "nombre"
+        ListBox1.ValueMember = "IdDepartamento"
+
+
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        'Dim cod As Integer = Val(ListBox1.SelectedValue.ToString)
+
+
+        'Dim table As New DataTable
+        'Dim sql As String = "SELECT nombrep FROM Rprovincias WHERE Idregion = " & cod
+        'Dim adp As New OleDbDataAdapter(sql, con)
+        'adp.Fill(table)
+
+        'ListBox2.DataSource = table
+        'ListBox2.DisplayMember = "nombrep"
+
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+       
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedValueChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedValueChanged
+        Dim cod As Integer = Val(ComboBox1.SelectedValue.ToString)
+
+
+
+        Dim table As New DataTable
+        Dim sql As String = "SELECT nombre FROM distritos WHERE IdDepartamento = " & cod
+        Dim adp As New OleDbDataAdapter(sql, con)
+        adp.Fill(table)
+
+        ComboBox2.DataSource = table
+        ComboBox2.DisplayMember = "nombre"
+    End Sub
+
+    
+    Private Sub ListBox1_SelectedValueChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedValueChanged
+
+        Dim cod As Integer = Val(ListBox1.SelectedValue.ToString)
+
+
+        Dim table As New DataTable
+        Dim sql As String = "SELECT nombre FROM distritos WHERE IdDepartamento = " & cod
+        Dim adp As New OleDbDataAdapter(sql, con)
+        adp.Fill(table)
+
+        ListBox2.DataSource = table
+        ListBox2.DisplayMember = "nombre"
+    End Sub
+End Class
